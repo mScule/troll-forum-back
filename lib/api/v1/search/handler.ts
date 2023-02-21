04658370add6203f33e9a3f9e1777fec30046a73
Vilhelm Niemi <54552657+mScule@users.noreply.github.com>
2023-02-21 17:51:06 +0200

    Now there's search route for searching users, posts, and comments

---
+++ lib/api/v1/search/handler.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express"
+import prisma from "../../../setup/prisma"
+import exclude from "../../../prisma/exclude"
+
+const get = async (req: Request, res: Response) => {
+  const searchValue = req.query.value + ""
+
+  await prisma(async client => {
+    const users = await client.user.findMany({
+      where: {
+        username: { contains: searchValue, mode: "insensitive" }
+      }
+    })
+
+    const posts = await client.post.findMany({
+      where: {
+        title: { contains: searchValue, mode: "insensitive"  }
+      }
+    })
+
+    const comments = await client.comment.findMany({
+      where: {
+        body: { contains: searchValue, mode: "insensitive"  }
+      }
+    })
+
+    res
+      .status(200)
+      .send({ users: exclude(users, ["password"]), posts, comments })
+  })
+}
+
+export default {
+  get
+}
